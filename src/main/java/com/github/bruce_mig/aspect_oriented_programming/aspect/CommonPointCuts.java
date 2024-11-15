8c1dcdd55f2858afe6054e25f59aca0683c41cea
bruce-mig <brucetmig@icloud.com>
2024-11-15 13:56:09 +0200

    added class to centralize common point cuts

---
+++ src/main/java/com/github/bruce_mig/aspect_oriented_programming/aspect/CommonPointCuts.java
@@ -0,0 +1,55 @@
+package com.github.bruce_mig.aspect_oriented_programming.aspect;
+
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class CommonPointCuts {
+
+    /**
+     * A join point is in the web layer if the method is defined
+     * in a type in the com.github.bruce_mig.aspect_oriented_programming.controller package or any sub-package
+     * under that.
+     */
+    @Pointcut("within(com.github.bruce_mig.aspect_oriented_programming.controller..*)")
+    public void inWebLayer() {}
+
+    /**
+     * A join point is in the service layer if the method is defined
+     * in a type in the com.github.bruce_mig.aspect_oriented_programming.service package or any sub-package
+     * under that.
+     */
+    @Pointcut("within(com.github.bruce_mig.aspect_oriented_programming.service..*)")
+    public void inServiceLayer() {}
+
+    /**
+     * A join point is in the data access layer if the method is defined
+     * in a type in the com.github.bruce_mig.aspect_oriented_programming.model package or any sub-package
+     * under that.
+     */
+    @Pointcut("within(com.github.bruce_mig.aspect_oriented_programming.model..*)")
+    public void inDataAccessLayer() {}
+
+
+    @Pointcut("execution(* com.github.bruce_mig.aspect_oriented_programming.service.UserService.*(..))")
+    public void methodsInServiceInterface() {}
+
+    /**
+     * A data access operation is the execution of any method defined on a
+     * DAO interface. This definition assumes that interfaces are placed in the
+     * "dao" package, and that implementation types are in sub-packages.
+     */
+    @Pointcut("execution(* com.xyz.dao.*.*(..))")
+    public void dataAccessOperation() {}
+
+     /**
+     * Pointcut for sensitive createUser method in UserService
+      */
+    @Pointcut("execution(* com.github.bruce_mig.aspect_oriented_programming.service.UserService.createUser(..))")
+    public void sensitiveCreateUserMethod() {}
+
+    /**
+     * Pointcut excluding the sensitive method
+    */
+    @Pointcut("methodsInServiceInterface() && !sensitiveCreateUserMethod()")
+    public void serviceLayerExcludingSensitive() {}
+}
