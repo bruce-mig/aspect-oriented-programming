c0b9cf7eab647443fa2d76c769c23a37f639f0f5
bruce-mig <brucetmig@icloud.com>
2024-11-15 15:16:33 +0200

    added custom annotation for sensitive methods

---
+++ src/main/java/com/github/bruce_mig/aspect_oriented_programming/annotations/SensitiveMethod.java
@@ -0,0 +1,11 @@
+package com.github.bruce_mig.aspect_oriented_programming.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SensitiveMethod {
+}
